021b376efab0ef90fa78ee52eeea3faa1c9f51cd
ksamarin90 <ksamarin90@gmail.com>
2024-02-23 11:08:11 +0400

    monorepo like

--- week1/hardhat.config.ts
+++ week1/hardhat.config.ts
@@ -1,17 +1,2 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-toolbox';
-
-const config: HardhatUserConfig = {
-    solidity: {
-        compilers: [
-            {
-                version: '0.8.20',
-            },
-            {
-                version: '0.4.17',
-            },
-        ],
-    },
-};
-
+import { config } from '../hardhat.config';
 export default config;
